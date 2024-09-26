38b3fe8eb0211976f9bcaee5b010e36369851091
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2024-09-26 22:10:26 +0600

    Handle password hide and show function completed

--- utils/togglePasswordInputType.js
+++ utils/togglePasswordInputType.js
@@ -1,7 +1,16 @@
-import { passwordInput } from "../lib/element/element.js";
+import { passwordCloseIcon, passwordInput } from "../lib/element/element.js";
+import { setStyle } from "./DomOperations.js";
 
-export default function togglePasswordInputType({ type }) {
+export default function togglePasswordInputType({ action }) {
   const type = passwordInput.type;
+
+  if (action === "generate" || type === "password") {
+    passwordInput.type = "text";
+    setStyle(passwordCloseIcon, { display: "block" });
+  } else {
+    passwordInput.type = "password";
+    setStyle(passwordCloseIcon, { display: "none" });
+  }
 
   console.log(type, "password type");
 }
